f37e50b72fb3319e7d1f6989a575e9882b6552bf
Thathsarani-Sandarekha <108277335+Thathsarani-Sandarekha@users.noreply.github.com>
2023-08-20 14:53:28 +0530

    Add files via upload

--- Infinity_Intergalactic_App/App.js
+++ Infinity_Intergalactic_App/App.js
@@ -1,14 +1,15 @@
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import StackNavigation from "./navigations/stack.navigation";
 import DrawerNavigator from './navigations/drawer.navigation'; // Your DrawerNavigator component
+import StackNavigation from './navigations/stack.navigation'; // Your StackNavigation component
+
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigation/>
+      <DrawerNavigator/>
     </NavigationContainer>
   );
 };
