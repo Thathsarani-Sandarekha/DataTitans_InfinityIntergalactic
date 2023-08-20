308598580fb6faf99537db7b1466b8b0c540e23d
Thathsarani-Sandarekha <108277335+Thathsarani-Sandarekha@users.noreply.github.com>
2023-08-20 12:29:58 +0530

    Add files via upload

---
+++ Backend/Booking.js
@@ -0,0 +1,26 @@
+// import { BookingPage } from "../screens";
+// import * as Booking from "../screens/Booking";
+
+const response = require('../screens/responseValues'); // Import the response object
+
+// const query = {
+//     'from' : 'planet',
+//     'to' : 'planet2',
+//     'startDate' : '2023-08-20 18:00:00.324',
+//     'returntDate' : '2023-08-24 18:00:00.324',
+//     'passengers' : 3,
+//     'class' : 'class'
+// }
+
+// const query = Booking.Booking.
+// function print(query){
+//     const { from, to, startDate, returnDate, passengers, class } = query;
+//     console.log(from);
+// }
+
+// print(query);
+
+// You can access the stored values
+console.log(response.planetName);
+console.log(response.fromLocation);
+// ... (other properties)
