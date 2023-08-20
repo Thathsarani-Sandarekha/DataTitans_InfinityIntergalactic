2152d32f44f6ddfe262a211e83f20f122e3c3efb
osheng7 <oshengeenath2002@gmail.com>
2023-08-20 10:28:39 +0530

    Planets screen completed

--- Infinity_Intergalactic_App/screens/PlanetShortDesc.jsx
+++ Infinity_Intergalactic_App/screens/PlanetShortDesc.jsx
@@ -2,11 +2,18 @@ const PlanetShortDesc = [
     {
         category: 'Recent destination',
         name: 'Planet 1',
-        description: 'Description 1',
+        description: 'Description 10',
         distance: '550KM',
         duration: '15 Days',
         price: '$100',
+        trip: '/ per trip',
         image: require('../assets/images/p1.jpg'),
+        styles: {
+            nameText: { position: 'absolute', top: 10, left: '70%', color: 'white', fontSize: 18 },
+            description: {color: '#A7A9BE',left: '90%',bottom: "20%" ,fontSize: 12 },
+            distance: { color: 'white', fontSize: 15,left: "75%", top: '70%' },
+            price: { color: '#00B9C2', fontSize: 18,top: "150%", right: "60%" },
+        },
     },
 
     {
@@ -17,6 +24,12 @@ const PlanetShortDesc = [
         duration: '15 Days',
         price: '$100',
         image: require('../assets/images/p2.jpg'),
+        styles: {
+            nameText: { position: 'absolute', top: 10, left: '70%', color: 'white', fontSize: 18 },
+            description: {color: '#A7A9BE',left: '90%',bottom: "20%" ,fontSize: 12 },
+            distance: { color: 'white', fontSize: 15,left: "75%", top: '70%' },
+            price: { color: '#00B9C2', fontSize: 18,top: "150%", right: "60%" },
+        },
     },
 
     {
@@ -27,6 +40,12 @@ const PlanetShortDesc = [
         duration: '15 Days',
         price: '$100',
         image: require('../assets/images/p3.jpg'),
+        styles: {
+            nameText: { position: 'absolute', top: 10, left: '70%', color: 'white', fontSize: 18 },
+            description: {color: '#A7A9BE',left: '90%',bottom: "20%" ,fontSize: 12 },
+            distance: { color: 'white', fontSize: 15,left: "75%", top: '70%' },
+            price: { color: '#00B9C2', fontSize: 18,top: "150%", right: "60%" },
+        },
     },
 
     {
@@ -37,6 +56,12 @@ const PlanetShortDesc = [
         duration: '15 Days',
         price: '$100',
         image: require('../assets/images/p4.jpg'),
+        styles: {
+            nameText: { position: 'absolute', top: 10, left: '70%', color: 'white', fontSize: 18 },
+            description: {color: '#A7A9BE',left: '90%',bottom: "20%" ,fontSize: 12 },
+            distance: { color: 'white', fontSize: 15,left: "75%", top: '70%' },
+            price: { color: '#00B9C2', fontSize: 18,top: "150%", right: "60%" },
+        },
     },
 
     {
@@ -47,66 +72,102 @@ const PlanetShortDesc = [
         duration: '15 Days',
         price: '$100',
         image: require('../assets/images/p5.jpg'),
+        styles: {
+            nameText: { position: 'absolute', top: 10, left: '70%', color: 'white', fontSize: 18 },
+            description: {color: '#A7A9BE',left: '90%',bottom: "20%" ,fontSize: 12 },
+            distance: { color: 'white', fontSize: 15,left: "75%", top: '70%' },
+            price: { color: '#00B9C2', fontSize: 18,top: "150%", right: "60%" },
+        },
     },
 
     {
         category: 'Popular destinations',
         name: 'Planet 1',
         image: require('../assets/images/p6.jpg'),
+        styles: {
+            nameText: { fontSize: 18, fontWeight: 'bold', color: 'white' },
+        },
     },
 
     {
         category: 'Popular destinations',
         name: 'Planet 2',
         image: require('../assets/images/p7.jpg'),
+        styles: {
+            nameText: { fontSize: 18, fontWeight: 'bold', color: 'white' },
+        },
     },
 
     {
         category: 'Popular destinations',
         name: 'Planet 3',
         image: require('../assets/images/p8.jpg'),
+        styles: {
+            nameText: { fontSize: 18, fontWeight: 'bold', color: 'white' },
+        },
     },
 
     {
         category: 'Popular destinations',
         name: 'Planet 4',
         image: require('../assets/images/p10.jpg'),
+        styles: {
+            nameText: { fontSize: 18, fontWeight: 'bold', color: 'white' },
+        },
     },
 
     {
         category: 'Popular destinations',
         name: 'Planet 5',
         image: require('../assets/images/p1.jpg'),
+        styles: {
+            nameText: { fontSize: 18, fontWeight: 'bold', color: 'white' },
+        },
     },
 
     {
         category: 'All destinations',
         name: 'Planet 1',
         image: require('../assets/images/p1.jpg'),
+        styles: {
+            nameText: { fontSize: 18, fontWeight: 'bold', color: 'white' },
+        },
     },
 
     {
         category: 'All destinations',
         name: 'Planet 2',
         image: require('../assets/images/p2.jpg'),
+        styles: {
+            nameText: { fontSize: 18, fontWeight: 'bold', color: 'white' },
+        },
     },
 
     {
         category: 'All destinations',
         name: 'Planet 3',
         image: require('../assets/images/p3.jpg'),
+        styles: {
+            nameText: { fontSize: 18, fontWeight: 'bold', color: 'white' },
+        },
     },
 
     {
         category: 'All destinations',
         name: 'Planet 4',
         image: require('../assets/images/p4.jpg'),
+        styles: {
+            nameText: { fontSize: 18, fontWeight: 'bold', color: 'white' },
+        },
     },
 
     {
         category: 'All destinations',
         name: 'Planet 5',
         image: require('../assets/images/p5.jpg'),
+        styles: {
+            nameText: { fontSize: 18, fontWeight: 'bold', color: 'white' },
+        },
     },
     
   ];
--- Infinity_Intergalactic_App/screens/Planets.jsx
+++ Infinity_Intergalactic_App/screens/Planets.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { View, Text, FlatList, Image, TouchableOpacity, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { PlanetShortDesc } from './PlanetShortDesc';// Import the image data
+import { PlanetShortDesc } from './PlanetShortDesc'; // Import the image data
+
+import backgroundImage from '../assets/images/mainbg.jpg'; // Import the background image
 
 export default function Planets() {
   const navigation = useNavigation();
@@ -15,37 +17,43 @@ export default function Planets() {
       item.category === 'Recent destination'
         ? { width: 300, height: 300 }
         : { width: 200, height: 200 };
-  
+
     return (
       <TouchableOpacity onPress={() => navigateToDescription(item.name)}>
         <View style={{ alignItems: 'center', justifyContent: 'center', position: 'relative' }}>
           <Image source={item.image} style={[imageStyle, { margin: 10 }]} resizeMode="cover" />
-          
-          {/* Name */}
-          <Text style={{ position: 'absolute', top: 10, left: 10, color: 'white', fontSize: 16 }}>
-            {item.name}
-          </Text>
-          
+
+        {/* Name */}
+        <Text style={item.styles?.nameText}>{item.name}</Text>
+
           {/* Small Description */}
           {item.category === 'Recent destination' && (
-            <View style={{ position: 'absolute', top: '50%', left: '50%', transform: [{ translateX: -150 }, { translateY: -15 }], backgroundColor: 'rgba(0, 0, 0, 0.5)', padding: 10 }}>
-              <Text style={{ color: 'white', fontSize: 14 }}>{item.description}</Text>
-              <Text style={{ color: 'white', fontSize: 12 }}>{`${item.distance} | ${item.duration}`}</Text>
-              <Text style={{ alignSelf: 'flex-end', color: 'white', fontSize: 14 }}>{item.price}</Text>
+            <View
+              style={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: [{ translateX: -150 }, { translateY: -15 }],
+                backgroundColor: 'transparent',
+                padding: 10,
+              }}
+            >
+            <Text style={item.styles.description}>{item.description}</Text>
+            <Text style={item.styles.distance}>{`${item.distance} | ${item.duration}`}</Text>
+            <Text style={{ alignSelf: 'flex-end', ...item.styles.price }}>{item.price}</Text>
             </View>
           )}
         </View>
       </TouchableOpacity>
     );
   };
-  
-  
 
   return (
     <ScrollView style={{ flex: 1 }}>
+      <Image source={backgroundImage} style={{width: '20px',height: '20px' ,position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }} resizeMode="cover" />
       <View style={{ paddingHorizontal: 20 }}>
         {/* Slider 1 */}
-        <Text style={{ fontSize: 20, fontWeight: 'bold', marginVertical: 10 }}>Recent Destinations</Text>
+        <Text style={{fontSize: 20, fontWeight: 'bold', marginVertical: 10, color: '#A6A6A6' }}>Recent Destinations</Text>
         <FlatList
           data={PlanetShortDesc.filter((item) => item.category === 'Recent destination')}
           horizontal
@@ -55,7 +63,7 @@ export default function Planets() {
         />
 
         {/* Slider 2 */}
-        <Text style={{ fontSize: 20, fontWeight: 'bold', marginVertical: 10 }}>Popular Destinations</Text>
+        <Text style={{color: '#A6A6A6', fontSize: 20, fontWeight: 'bold', marginVertical: 10, color: '#A6A6A6' }}>Popular Destinations</Text>
         <FlatList
           data={PlanetShortDesc.filter((item) => item.category === 'Popular destinations')}
           horizontal
@@ -65,7 +73,7 @@ export default function Planets() {
         />
 
         {/* Slider 3 */}
-        <Text style={{ fontSize: 20, fontWeight: 'bold', marginVertical: 10 }}>All Destinations</Text>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', marginVertical: 10, color: '#A6A6A6' }}>All Destinations</Text>
         <FlatList
           data={PlanetShortDesc.filter((item) => item.category === 'All destinations')}
           horizontal
